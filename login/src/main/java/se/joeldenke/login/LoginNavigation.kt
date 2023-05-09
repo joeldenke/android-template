7e9c095dad06e4b177c2cfc61ec495700b899984
Joel Denke <joel@denke.se>
2023-05-09 17:39:32 +0200

    Added morph animation

--- login/src/main/java/se/joeldenke/login/LoginNavigation.kt
+++ login/src/main/java/se/joeldenke/login/LoginNavigation.kt
@@ -1,13 +1,23 @@
 package se.joeldenke.login
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.spring
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.Morph
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.Star
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import kotlinx.coroutines.launch
 
 sealed interface LoginNavEntry {
     val destination: String
@@ -18,13 +28,31 @@ sealed interface LoginNavEntry {
             get() = {
                 val viewModel = viewModel<UsernameLoginViewModel>()
                 val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+                val progress = remember { Animatable(0f) }
+                val pointyTriangle = RoundedPolygon(3)
+                val roundedStar = Star(numVerticesPerRadius = 5, innerRadius = .5f, rounding = CornerRounding(radius = .1f))
+                val morph = Morph(pointyTriangle, roundedStar)
+                val sizedMorph = SizedMorph(morph)
+                val coroutineScope = rememberCoroutineScope()
                 UsernameLoginScreen(
                     uiState = uiState,
                     onUsernameChanged = { viewModel.updateUsername(it) },
-                    onPasswordChanged = { viewModel.updatePassword(it) }
-                )
+                    onPasswordChanged = { viewModel.updatePassword(it) },
+                    sizedMorph = sizedMorph,
+                    prep = {
+                        sizedMorph.morph.progress = progress.value
+                    }
+                ) { coroutineScope.launch { doAnimation(progress = progress) }}
             }
     }
+}
+
+private suspend fun doAnimation(progress: Animatable<Float, AnimationVector1D>) {
+    val end = if (progress.targetValue == 1f) 0f else 1f
+    progress.animateTo(
+        end,
+        animationSpec = spring(0.6f, 50f)
+    )
 }
 
 fun NavGraphBuilder.loginGraph() {
--- login/src/main/java/se/joeldenke/login/UsernameLoginScreen.kt
+++ login/src/main/java/se/joeldenke/login/UsernameLoginScreen.kt
@@ -1,24 +1,25 @@
 package se.joeldenke.login
 
-import android.graphics.drawable.Icon
+import android.graphics.Matrix
+import android.graphics.RectF
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.graphics.shapes.Morph
+import androidx.graphics.shapes.RoundedPolygon
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
@@ -26,6 +27,8 @@ import se.joeldenke.theme.ui.component.JText
 import se.joeldenke.theme.ui.component.JTextField
 import se.joeldenke.theme.ui.component.JTextResource
 import se.joeldenke.theme.ui.theme.JDesignSystem
+import kotlin.math.abs
+import kotlin.math.min
 
 data class UsernameUiState(
     val username: String = "",
@@ -44,11 +47,39 @@ class UsernameLoginViewModel: ViewModel() {
     val uiState = MutableStateFlow(UsernameUiState())
 }
 
+internal fun calculateMatrix(bounds: RectF, width: Float, height: Float): Matrix {
+    val originalWidth = bounds.right - bounds.left
+    val originalHeight = bounds.bottom - bounds.top
+    val scale = min(width / originalWidth, height / originalHeight)
+    val newLeft = bounds.left - (width / scale - originalWidth) / 2
+    val newTop = bounds.top - (height / scale - originalHeight) / 2
+    val matrix = Matrix()
+    matrix.setTranslate(-newLeft, -newTop)
+    matrix.postScale(scale, scale)
+    return matrix
+}
+class SizedMorph(val morph: Morph) {
+    var width = 1f
+    var height = 1f
+
+    fun resizeMaybe(newWidth: Float, newHeight: Float) {
+        if (abs(width - newWidth) > 1e-4 || abs(height - newHeight) > 1e-4) {
+            val matrix = calculateMatrix(RectF(0f, 0f, width, height), newWidth, newHeight)
+            morph.transform(matrix)
+            width = newWidth
+            height = newHeight
+        }
+    }
+}
+
 @Composable
 fun UsernameLoginScreen(
     uiState: UsernameUiState,
     onUsernameChanged: (String) -> Unit,
     onPasswordChanged: (String) -> Unit,
+    prep: ContentDrawScope.() -> Unit = {},
+    sizedMorph: SizedMorph,
+    onSend: () -> Unit
 ) {
     Column {
         JText(text = JTextResource.Text("Test login"), style = JDesignSystem.typography.h2)
@@ -63,6 +94,25 @@ fun UsernameLoginScreen(
             onValueChanged = onPasswordChanged,
             transformation = PasswordVisualTransformation(),
         )
+        Box(modifier = Modifier
+            .background(
+                JDesignSystem.colorTheme.primary,
+                shape = RoundedCornerShape(50)
+            )
+            .drawWithContent {
+                prep()
+                drawContent()
+                sizedMorph.resizeMaybe(size.width, size.height)
+                drawPath(sizedMorph.morph.asPath().asComposePath(), Color.White)
+            }
+            .clickable {
+                onSend()
+            }
+        ) {
+            Row(Modifier.padding(vertical = 8.dp, horizontal = 12.dp)) {
+                JText(text = JTextResource.Text("Send"))
+            }
+        }
     }
 }
 
@@ -72,6 +122,7 @@ fun UsernameLoginScreenPreview() {
     UsernameLoginScreen(
         uiState = UsernameUiState("test", "none"),
         onUsernameChanged = {},
-        onPasswordChanged = {}
-    )
+        onPasswordChanged = {},
+        sizedMorph = SizedMorph(Morph(RoundedPolygon(3), RoundedPolygon(5))),
+    ) { }
 }
